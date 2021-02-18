da569a345c97eb242f85e09a4e3a1d5070fae7c1
r3nanp <renanmol87@gmail.com>
2021-02-18 12:19:04 -0300

    feat(app): add leaflet styles and add type myapp function

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,12 @@
+import 'leaflet/dist/leaflet.css'
+// eslint-disable-next-line no-use-before-define
+import React from 'react'
 import { AppProps } from 'next/app'
 import { ThemeProvider } from 'styled-components'
 import GlobalStyles from '../styles/global'
 import { theme } from '../styles/theme'
 
-function MyApp({ Component, pageProps }: AppProps) {
+function MyApp({ Component, pageProps }: AppProps): JSX.Element {
   return (
     <ThemeProvider theme={theme}>
       <GlobalStyles />
