e12627da4beda25d472a898ff29c54400be704cf
r3nanp <renanmol87@gmail.com>
2021-02-19 14:16:29 -0300

    feat(index): add input and handle submit function

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,26 +1,84 @@
 // eslint-disable-next-line no-use-before-define
-import React, { FormEvent, useCallback } from 'react'
+import React, { useCallback, useRef, useState } from 'react'
 import Head from 'next/head'
 import Image from 'next/image'
 import { useRouter } from 'next/router'
+import { FormHandles, SubmitHandler } from '@unform/core'
+import * as yup from 'yup'
 
-import { Form } from '../components/Form'
-import { Wrapper } from '../components/Wrapper'
+import { Form, Wrapper } from '../components/Wrapper'
+import { Input } from '../components/Input'
+
+interface HomeProps {
+  email: string
+  password: string
+}
 
 export default function Home(): JSX.Element {
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+
+  const formRef = useRef<FormHandles>(null)
+
   const router = useRouter()
 
-  const handleSubmit = useCallback((event: FormEvent) => {
-    event.preventDefault()
+  const handleSubmit: SubmitHandler<HomeProps> = useCallback(
+    async (data, { reset }, event) => {
+      try {
+        formRef.current.setErrors({})
 
-    router.push('/map')
-  }, [])
+        const schema = yup.object().shape({
+          username: yup.string().min(5).max(60).required(),
+          password: yup.string().min(6).required()
+        })
+
+        await schema.validate(data, {
+          abortEarly: false
+        })
+
+        router.push('/map')
+        event.preventDefault()
+        reset()
+      } catch (error) {
+        const validationErrors = {}
+        if (error instanceof yup.ValidationError) {
+          error.inner.forEach(error => {
+            validationErrors[error.path] = error.message
+          })
+          formRef.current.setErrors(validationErrors)
+        }
+      }
+    },
+    []
+  )
 
   return (
     <Wrapper>
       <Head>
         <title>Delivery</title>
         <link rel="icon" href="/package.svg" />
+
+        <meta name="keywords" content="r3nanp, deliveries, packages" />
+        <meta property="og:site_name" content="Delivery" />
+        <meta property="og:title" content="Delivery" />
+        <meta
+          property="og:description"
+          content="A delivery app to your favorite products"
+        />
+
+        <meta property="og:image" content="/package.svg" />
+        <meta property="og:image:type" content="image/png" />
+
+        <meta property="og:image:width" content="1200" />
+        <meta property="og:image:height" content="630" />
+
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content="Delivery" />
+        <meta
+          name="twitter:description"
+          content="A delivery app to your favorite products"
+        />
+        <meta name="twitter:image" content="/package.svg" />
       </Head>
 
       <div className="image-container">
@@ -32,15 +90,25 @@ export default function Home(): JSX.Element {
         />
       </div>
 
-      <Form onSubmit={handleSubmit}>
+      <Form ref={formRef} onSubmit={handleSubmit}>
         <fieldset>
           <legend>Entre e comece a pedir agora!</legend>
 
-          <label htmlFor="email">Digite seu email</label>
-          <input type="text" name="email" required />
+          <label htmlFor="username">Digite seu nome de usuário</label>
+          <Input
+            type="text"
+            name="username"
+            value={username}
+            onChange={event => setUsername(event.target.value)}
+          />
 
           <label htmlFor="password">Digite sua senha</label>
-          <input type="password" name="password" required />
+          <Input
+            type="password"
+            name="password"
+            value={password}
+            onChange={event => setPassword(event.target.value)}
+          />
         </fieldset>
 
         <button>Comece a pedir</button>
