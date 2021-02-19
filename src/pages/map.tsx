fb454befc95f253fe17f82aee46b29ec8ec425bd
r3nanp <renanmol87@gmail.com>
2021-02-18 21:30:13 -0300

    feat(map): add animation and header components

--- src/pages/map.tsx
+++ src/pages/map.tsx
@@ -3,21 +3,27 @@ import React from 'react'
 import Head from 'next/head'
 import dynamic from 'next/dynamic'
 
+import { Animation } from '../components/Animation'
+import { Header } from '../components/Header'
+
 export default function Map(): JSX.Element {
+  const displayAnimation = <Animation />
+
   const MapContainerWithNoSSR = dynamic(() => import('../components/Map'), {
-    loading: () => <p>Loading Map</p>,
+    loading: () => displayAnimation,
     ssr: false
   })
 
   return (
-    <main>
+    <>
       <Head>
         <title>Deliveries!</title>
         <meta name="keywords" content="maps, deliveries" />
         <link rel="shortcut icon" href="/package.svg" />
       </Head>
 
+      <Header />
       <MapContainerWithNoSSR />
-    </main>
+    </>
   )
 }
