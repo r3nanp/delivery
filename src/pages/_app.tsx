004acea97d76bf77135fb4f076ea7a9307a78e26
r3nanp <renanmol87@gmail.com>
2021-02-19 14:15:04 -0300

    feat(app): add service worker when page mounts

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,25 @@
 import 'leaflet/dist/leaflet.css'
 // eslint-disable-next-line no-use-before-define
-import React from 'react'
+import React, { useEffect } from 'react'
 import { AppProps } from 'next/app'
 import { ThemeProvider } from 'styled-components'
 import GlobalStyles from '../styles/global'
 import { theme } from '../styles/theme'
 
 function MyApp({ Component, pageProps }: AppProps): JSX.Element {
+  useEffect(() => {
+    if ('serviceWorker' in navigator) {
+      navigator.serviceWorker
+        .register('/serviceworker.js')
+        .then(() => {
+          console.log('service worker registered')
+        })
+        .catch(() => {
+          console.warn('service worker failed')
+        })
+    }
+  })
+
   return (
     <ThemeProvider theme={theme}>
       <GlobalStyles />
